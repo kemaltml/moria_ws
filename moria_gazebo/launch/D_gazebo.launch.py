72470e1464db3a08245c11d63cadef53b500a571
kemaltml <kemaltmll@proton.me>
2024-11-13 15:25:18 +0300

    Here the new packages

---
+++ moria_gazebo/launch/D_gazebo.launch.py
@@ -0,0 +1,71 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('moria_gazebo')
+    urdf_file = os.path.join(pkg_path, 'model', 'moria.urdf')
+
+    with open(urdf_file, 'r') as infp:
+        robot_description = infp.read()
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'verbose': 'true', 'factory': 'true'}.items(),
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros', executable='spawn_entity.py',
+        arguments=['-file', urdf_file, '-entity', 'robot'],
+        output='screen'
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    controller_manager = Node(
+        package='gazebo_ros2_control',
+        executable='gazebo_ros2_control',
+        name='gazebo_ros2_control',
+        parameters=[
+            '/home/kemaltml/moria_ws/install/moria_gazebo/share/moria_gazebo/config/my_controllers.yaml',
+            '/home/kemaltml/moria_ws/install/moria_gazebo/share/moria_gazebo/config/gaz_ros2_ctl_use_sim.yaml'
+            ],
+        output='screen'
+    )
+
+    # Ensure `spawn_entity` runs after `controller_manager` starts
+    delayed_spawn_entity = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[spawn_entity],
+        )
+    )
+
+    
+
+    return LaunchDescription([
+        gazebo,
+        controller_manager,
+        delayed_spawn_entity,
+        joint_state_publisher,
+        robot_state_publisher,
+    ])
+
---
+++ moria_gazebo/launch/btu.launch.py
@@ -0,0 +1,119 @@
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    launch_file_dir = os.path.join(get_package_share_directory('moria_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(get_package_share_directory('moria_gazebo'),
+                        'worlds',
+                        'btu.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                pkg_gazebo_ros, 
+                'launch', 
+                'gzserver.launch.py'
+            ),
+        ]),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                pkg_gazebo_ros, 
+                'launch',
+                'gzclient.launch.py'
+            )
+        ])
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                launch_file_dir,
+                'robot_state_publisher.launch.py'
+            )
+        ]),
+        launch_arguments={'use_sim_time': use_sim_time, 'use_ros2_control': 'true'}.items()
+    )
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('moria_gazebo'),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory('moria_gazebo'),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    )
+
+    spawn_moria_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                launch_file_dir,
+                'spawn_moria.launch.py'
+            )
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        output="screen"
+    )
+
+    delayed_spawn_entity = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[spawn_moria_cmd],
+        )
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(joystick)
+    ld.add_action(twist_mux)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(controller_manager)
+    ld.add_action(delayed_spawn_entity)
+    ld.add_action(diff_drive_spawner)
+    ld.add_action(joint_broad_spawner)
+
+    return ld
