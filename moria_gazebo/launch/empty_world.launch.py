84ae3c4cd56f3bf659759df36673c99ee6479576
kemaltml <kemaltmll@proton.me>
2025-01-07 21:19:05 +0300

    Added gazebo simulation package. Not working so well. Will work on it.

---
+++ moria_gazebo/launch/empty_world.launch.py
@@ -0,0 +1,103 @@
+
+import os
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    launch_file_dir = os.path.join(get_package_share_directory('moria_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(get_package_share_directory('moria_gazebo'),
+                        'worlds',
+                        'empty_world.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                pkg_gazebo_ros, 
+                'launch', 
+                'gzserver.launch.py'
+            ),
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                pkg_gazebo_ros, 
+                'launch',
+                'gzclient.launch.py'
+            )
+        )
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                launch_file_dir,
+                'state_publisher.launch.py'
+            )
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('moria_gazebo'),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory('moria_gazebo'),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+    )
+
+    spawn_moria_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                launch_file_dir,
+                'spawn_moria.launch.py'
+            )
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_moria_cmd)
+    ld.add_action(joystick)
+    ld.add_action(twist_mux)
+    ld.add_action(diff_drive_spawner)
+    ld.add_action(joint_broad_spawner)
+
+    return ld
---
+++ moria_gazebo/launch/spawn_moria.launch.py
@@ -0,0 +1,42 @@
+import os
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    model_folder = 'models/moria'
+    urdf_file_name = 'model.urdf'
+    urdf_file_path = os.path.join(get_package_share_directory('moria_gazebo'),
+                                    model_folder, urdf_file_name)
+    
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    declare_x_position_cmd = DeclareLaunchArgument(
+                            'x_pose', default_value='0.0',
+                            description='Specify namespace of the robot')
+    
+    declare_y_position_cmd = DeclareLaunchArgument(
+                            'y_pose', default_value='0.0',
+                            description='Specify namespace of the robot')
+    
+    start_gazebo_ros_spawner = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'Moria',
+            '-file', urdf_file_path,
+            '-x', x_pose,
+            '-y', y_pose
+        ],
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(declare_x_position_cmd)
+    ld.add_action(declare_y_position_cmd)
+    ld.add_action(start_gazebo_ros_spawner)
+
+    return ld
